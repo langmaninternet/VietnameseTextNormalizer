b3ca25ba3f26ba6e018262ce3652b9f7ca52c249
Bùi Tấn Quang <langmaninternet@hotmail.com>
2018-10-02 00:24:20 +0700

    Sửa lại code test, thêm mô hình với từ viết tắt ko và dc
    tên file cũ đặt là UnitTestVietnameseTextNormalizer.so mà tên file mới là VietnameseTextNormalizer nên ko chạy,
    
    đổi lại code test cho phù hợp với tên mới

--- Build 2018 10 01 - Linux Python3/UnitTestVietnameseTextNormalizer.py
+++ Build 2018 10 01 - Linux Python3/UnitTestVietnameseTextNormalizer.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
-import UnitTestVietnameseTextNormalizer
-a=UnitTestVietnameseTextNormalizer.Normalize("Tôi làm việ ở ban công ngệ FPT, tôi là người viêt nam");
+import VietnameseTextNormalizer
+a=VietnameseTextNormalizer.Normalize("Tôi làm việ ở ban công ngệ FPT, tôi là người viêt nam. hôm nay tôi ko thích ăn mì tôm. tôi làm đc 2 bài tập.");
 print (a)
