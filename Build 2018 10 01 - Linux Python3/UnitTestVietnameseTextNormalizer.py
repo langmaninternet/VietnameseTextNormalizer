5e86f93fa475cab3874a915f2e821ec95607ae27
Bùi Tấn Quang <langmaninternet@hotmail.com>
2018-10-04 15:30:37 +0700

    tối ưu hóa bộ nhớ

--- Build 2018 10 01 - Linux Python3/UnitTestVietnameseTextNormalizer.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-import VietnameseTextNormalizer
-a=VietnameseTextNormalizer.Normalize("Tôi làm việ ở ban công ngệ FPT, tôi là người viêt nam. hôm nay tôi ko thích ăn mì tôm. tôi làm đc 2 bài tập.");
-print (a)
