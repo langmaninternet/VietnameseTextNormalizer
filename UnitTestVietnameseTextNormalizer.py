c87bf478a3182ef638c6eb0be422c2bc54a5957f
Bùi Tấn Quang <langmaninternet@gmail.com>
2021-02-18 15:38:53 +0700

    Update UnitTestVietnameseTextNormalizer.py

--- UnitTestVietnameseTextNormalizer.py
+++ UnitTestVietnameseTextNormalizer.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 import VietnameseTextNormalizer
-a=u"UCS2 : Tôi làm việ ở ban công ngệ FPT, tôi là người viêt nam. hôm nay tôi ko thích ăn mì tôm. tôi làm đc 2 bài tập."
-print (a)
+
 a=VietnameseTextNormalizer.Normalize(u"UCS2 : Tôi làm việ ở ban công ngệ FPT, tôi là người viêt nam. hôm nay tôi ko thích ăn mì tôm. tôi làm đc 2 bài tập.");
 print (a)
 print (type(a))
@@ -12,22 +11,22 @@ print (b)
 print (type(b))
 
 
-print ("Kì vọng  bằng: '1', huyền: '2', ngã: '3', hỏi: '4', sắc: '5', nặng: '6' ")
-print ("bằng  : " + VietnameseTextNormalizer.GetFirstTone("tôi"))
-print ("huyền : " + VietnameseTextNormalizer.GetFirstTone("huyền"))
-print ("ngã   : " + VietnameseTextNormalizer.GetFirstTone("ngã"))
-print ("hỏi   : " + VietnameseTextNormalizer.GetFirstTone("hỏi"))
-print ("sắc   : " + VietnameseTextNormalizer.GetFirstTone("sắc"))
-print ("nặng  : " + VietnameseTextNormalizer.GetFirstTone("nặng"))
-print ("Other : " + VietnameseTextNormalizer.GetFirstTone(''))
-print ("Other : " + VietnameseTextNormalizer.GetFirstTone(u''))
-
-
-a=VietnameseTextNormalizer.GetIBase(u"UCS2 : Tôi làm việ ở ban công ngệ FPT, tôi là người viêt nam. hôm nay tôi ko thích ăn mì tôm. tôi làm đc 2 bài tập.");
-print (a)
-print (type(a))
-
-
-b=VietnameseTextNormalizer.GetIBase(" UTF8 : Tôi làm việ ở ban công ngệ FPT, tôi là người viêt nam. hôm nay tôi ko thích ăn mì tôm. tôi làm đc 2 bài tập.");
-print (b)
-print (type(b))
+#   print ("Kì vọng  bằng: '1', huyền: '2', ngã: '3', hỏi: '4', sắc: '5', nặng: '6' ")
+#   print ("bằng  : " + VietnameseTextNormalizer.GetFirstTone("tôi"))
+#   print ("huyền : " + VietnameseTextNormalizer.GetFirstTone("huyền"))
+#   print ("ngã   : " + VietnameseTextNormalizer.GetFirstTone("ngã"))
+#   print ("hỏi   : " + VietnameseTextNormalizer.GetFirstTone("hỏi"))
+#   print ("sắc   : " + VietnameseTextNormalizer.GetFirstTone("sắc"))
+#   print ("nặng  : " + VietnameseTextNormalizer.GetFirstTone("nặng"))
+#   print ("Other : " + VietnameseTextNormalizer.GetFirstTone(''))
+#   print ("Other : " + VietnameseTextNormalizer.GetFirstTone(u''))
+#      
+#   
+#   a=VietnameseTextNormalizer.GetIBase(u"UCS2 : Tôi làm việ ở ban công ngệ FPT, tôi là người viêt nam. hôm nay tôi ko thích ăn mì tôm. tôi làm đc 2 bài tập.");
+#   print (a)
+#   print (type(a))
+#   
+#   
+#   b=VietnameseTextNormalizer.GetIBase(" UTF8 : Tôi làm việ ở ban công ngệ FPT, tôi là người viêt nam. hôm nay tôi ko thích ăn mì tôm. tôi làm đc 2 bài tập.");
+#   print (b)
+#   print (type(b))
