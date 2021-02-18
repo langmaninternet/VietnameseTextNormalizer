7430fde852e5346897de6af1b539668647cd5e4a
Bùi Tấn Quang <langmaninternet@gmail.com>
2021-02-18 11:08:19 +0700

    Update UnitTestVietnameseTextNormalizer.py

--- UnitTestVietnameseTextNormalizer.py
+++ UnitTestVietnameseTextNormalizer.py
@@ -2,7 +2,9 @@
 import VietnameseTextNormalizer
 a=VietnameseTextNormalizer.Normalize(u"UCS2 : Tôi làm việ ở ban công ngệ FPT, tôi là người viêt nam. hôm nay tôi ko thích ăn mì tôm. tôi làm đc 2 bài tập.");
 print (a)
+print (type(a))
 
 
-a=VietnameseTextNormalizer.Normalize(" UTF8 : Tôi làm việ ở ban công ngệ FPT, tôi là người viêt nam. hôm nay tôi ko thích ăn mì tôm. tôi làm đc 2 bài tập.");
-print (a)
+b=VietnameseTextNormalizer.Normalize(" UTF8 : Tôi làm việ ở ban công ngệ FPT, tôi là người viêt nam. hôm nay tôi ko thích ăn mì tôm. tôi làm đc 2 bài tập.");
+print (b)
+print (type(b))
